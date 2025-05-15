6bc225f4cc7cfaf51c47173a8700716f54fb7105
evishnevskiy <evgeniy.vishnevskiy@itrexgroup.com>
2025-05-15 14:25:04 +0200

    Initial commit.

---
+++ sleep/src/main/java/com/noom/interview/backend/sleep/SleepLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.noom.interview.backend.sleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepLoggerApplication.class, args);
+    }
+
+}
